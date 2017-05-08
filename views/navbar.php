a3e2cbc01f6fba16adaa412a663276ce35c76cca
Sanaulla <sanaulla.parvez@aamra.com.bd>
2017-05-08 13:12:45 +0600

    modify nav bar

--- views/navbar.php
+++ views/navbar.php
@@ -11,14 +11,16 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
 <!--                 <li><a href="#">Dashboard</a></li>
                 <li><a href="#">Settings</a></li>
                 <li><a href="#">Profile</a></li>
                 <li><a href="#">Help</a></li> -->
-        <li ng-class="{ active: isActive('/stone')}"><a ui-sref= "stone" >{{ 'STONE' | translate }}</a></li>
-        <li ng-class="{ active: isActive('/new_stone')}"><a ui-sref= "new_stone" >{{ 'NEW_STONE' | translate }}</a></li>
-        <li ng-class="{ active: isActive('/expense_sector')}"><a ui-sref= "expense_sector" >{{ 'EXPENSE_SECTOR' | translate }}</a></li>
-        <li ng-class="{ active: isActive('/new_expense_sector')}"><a ui-sref= "new_expense_sector" >{{ 'NEW_EXPENSE_SECTOR' | translate }}</a></li>
+                <li ng-class="{ active: isActive('/stone')}"><a ui-sref= "stone" >{{ 'STONE' | translate }}</a></li>
+                <li ng-class="{ active: isActive('/new_stone')}"><a ui-sref= "new_stone" >{{ 'NEW_STONE' | translate }}</a></li>
+                <li ng-class="{ active: isActive('/expense_sector')}"><a ui-sref= "expense_sector" >{{ 'EXPENSE_SECTOR' | translate }}</a></li>
+                <li ng-class="{ active: isActive('/new_expense_sector')}"><a ui-sref= "new_expense_sector" >{{ 'NEW_EXPENSE_SECTOR' | translate }}</a></li>
             </ul>
         </div>
     </div>
--- views/sidebar.php
+++ views/sidebar.php
@@ -1,21 +1,22 @@
  <a class="isSidebarOpen" href ng-click="isSidebarOpen = !isSidebarOpen"><span ng-class="{'glyphicon glyphicon-menu-down':isSidebarOpen,'glyphicon glyphicon-menu-up':!isSidebarOpen}" aria-hidden="true"></span></a>
 <div ng-show="isSidebarOpen" class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-        <li ng-class="{ active: isActive('/buyer')}"><a ui-sref= "buyer" >{{ 'BUYER' | translate }}</a></li>
-        <li ng-class="{ active: isActive('/new_buyer')}"><a ui-sref= "new_buyer" >{{ 'NEW_BUYER' | translate }}</a></li>
         <li ng-class="{ active: isActive('/new_sale')}"><a ui-sref= "new_sale" >{{ 'NEW_SALE' | translate }}</a></li>
-        <li ng-class="{ active: isActive('/sales')}"><a ui-sref= "sales" >{{ 'SALE' | translate }}</a></li>
         <li ng-class="{ active: isActive('/new_expense')}"><a ui-sref= "new_expense" >{{ 'NEW_EXPENSE' | translate }}</a></li>
+        <li ng-class="{ active: isActive('/new_buyer')}"><a ui-sref= "new_buyer" >{{ 'NEW_BUYER' | translate }}</a></li>
+        <li>********************************************</li>
+        <li ng-class="{ active: isActive('/sales')}"><a ui-sref= "sales" >{{ 'SALE' | translate }}</a></li>
         <li ng-class="{ active: isActive('/expenses')}"><a ui-sref= "expenses" >{{ 'EXPENSES' | translate }}</a></li>
-        <li>------------------------------------------</li>
-        <li ng-class="{ active: isActive('/seller')}"><a ui-sref= "seller" >{{ 'SELLER' | translate }}</a></li>
-        <li ng-class="{ active: isActive('/new_seller')}"><a ui-sref= "new_seller" >{{ 'NEW_SELLER' | translate }}</a></li>
+        <li ng-class="{ active: isActive('/buyer')}"><a ui-sref= "buyer" >{{ 'BUYER' | translate }}</a></li>
+        <li><b>=========================================</b></li>
         <li ng-class="{ active: isActive('/new_buy')}"><a ui-sref= "new_buy" >{{ 'NEW_BUY' | translate }}</a></li>
-        <li ng-class="{ active: isActive('/buy')}"><a ui-sref= "buy" >{{ 'BUY' | translate }}</a></li>
         <li ng-class="{ active: isActive('/new_income')}"><a ui-sref= "new_income" >{{ 'NEW_INCOME' | translate }}</a></li>
+        <li ng-class="{ active: isActive('/new_seller')}"><a ui-sref= "new_seller" >{{ 'NEW_SELLER' | translate }}</a></li>
+        <li>********************************************</li>
+        <li ng-class="{ active: isActive('/buy')}"><a ui-sref= "buy" >{{ 'BUY' | translate }}</a></li>
+        <li ng-class="{ active: isActive('/incomes')}"><a ui-sref= "incomes" >{{ 'INCOMES' | translate }}</a></li>
+        <li ng-class="{ active: isActive('/seller')}"><a ui-sref= "seller" >{{ 'SELLER' | translate }}</a></li>
         <!-- <li ng-class="{ active: isActive('/student')}"><a ui-sref= "student" >{{ 'STUDENT' | translate }}</a></li>
         <li ng-class="{ active: isActive('/student_admission')}"><a ui-sref= "student_admission" >{{ 'STUDENT_ADMISSION' | translate }}</a></li> -->
-        <li ng-class="{ active: isActive('/incomes')}"><a ui-sref= "incomes" >{{ 'INCOMES' | translate }}</a></li>
-
     </ul>
 </div>
